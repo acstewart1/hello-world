b22a229f0cb44569bd4ea40157751c36a602ab7b
Crag Stewart-McDougall <5arrowscraig@gmail.com>
2018-11-27 20:54:42 +1100

    tried some input commands

--- pythonTest.py
+++ pythonTest.py
@@ -1,2 +1,8 @@
-print "Hello World"
-print "Hello Frank"
+import datetime
+currentDT = datetime.datetime.now()
+print (str(currentDT),": Program is now starting")
+currentDT = datetime.datetime.now()
+NameAnswer = input("Hi, What is your name ?")
+currentDT = datetime.datetime.now()
+print (str(currentDT),": Okay, so your name is ",NameAnswer,", is that correct ?")
+ConfirmAnswer = input("Enter Y or N:")
